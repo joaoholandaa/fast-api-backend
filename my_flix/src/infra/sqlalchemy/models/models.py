94fcc39fc5d134e0ff4b29313076fd4c3acf0e1f
Joao Holanda <joaocmdt505@gmail.com>
2022-10-20 20:50:37 -0300

    up myflix app and .gitignore file

---
+++ my_flix/src/infra/sqlalchemy/models/models.py
@@ -0,0 +1,13 @@
+from email import generator
+from operator import index
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from src.infra.sqlalchemy.config.database import Base
+
+class Serie(Base):
+    __tablename__ = 'serie'
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String)
+    ano = Column(Integer)
+    genero = Column(String)
+    qtd_temporadas = Column(Integer)
