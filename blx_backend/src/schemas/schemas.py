09a61b7f0bbf7f6b0bed50be79951abb832d4ea6
Joao Holanda <joaocmdt505@gmail.com>
2022-10-26 01:51:50 -0300

    Modelos, estruturação, alembic e relacionamentos

---
+++ blx_backend/src/schemas/schemas.py
@@ -0,0 +1,33 @@
+from re import S
+from pydantic import BaseModel
+from typing import Optional, List
+
+class Usuario(BaseModel):
+    id: Optional[str] = None
+    nome: str
+    telefone: str
+
+class Produto(BaseModel):
+    id: Optional[str] = None
+    nome: str
+    detalhes: str
+    preco: float
+    disponivel: bool = False
+
+    class Config:
+        orm_mode = True
+
+class ProdutoSimples(BaseModel):
+    id: Optional[str] = None
+    nome: str
+    preco: float
+
+    class Config:
+        orm_mode = True
+
+class Pedido(BaseModel):
+    id:Optional[str] = None
+    quantidade: int
+    entrega: bool = True
+    endereco: str
+    observacoes: Optional[str] = 'Sem observações'
